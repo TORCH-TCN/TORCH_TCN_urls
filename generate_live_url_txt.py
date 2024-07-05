dadb050974cb9ba0b6a7c1cc7b3672424955001a
Jason Best <jbest@brit.org>
2024-07-05 16:31:32 -0500

    added txt files, update

--- generate_live_url_txt.py
+++ generate_live_url_txt.py
@@ -5,10 +5,10 @@ import requests
 
 with open('torch_urls.csv') as f:
     for row in csv.DictReader(f):
-        if row['in_tx_ok'] =='TRUE':
-            if row['dwca_url_active'] == 'FALSE':
-                if row['dwca_live_url_active'] =='TRUE':
-                    url = row['dwca_live_url']
-                    print(url)
+        #if row['in_tx_ok'] =='TRUE':
+        if row['dwca_url_active'] == 'FALSE':
+            if row['dwca_live_url_active'] =='TRUE':
+                url = row['dwca_live_url']
+                print(url)
 
 
--- generate_url_txt.py
+++ generate_url_txt.py
@@ -5,12 +5,12 @@ import requests
 
 with open('torch_urls.csv') as f:
     for row in csv.DictReader(f):
-        if row['in_tx_ok'] =='TRUE':
-            if row['dwca_url_active'] == 'TRUE':
-                url = row['dwca_url']
-                print(url)
-            elif row['ipt_url']:
-                url = row['ipt_url']
-                print(url)
+        #if row['in_tx_ok'] =='TRUE':
+        if row['dwca_url_active'] == 'TRUE':
+            url = row['dwca_url']
+            print(url)
+        elif row['ipt_url']:
+            url = row['ipt_url']
+            print(url)
 
 
