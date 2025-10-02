314378fdeef19cbae820d53090b4d6c5a083ac2a
Jason Best <jbest@brit.org>
2025-10-02 15:34:10 -0500

    added header to prevent 403 error

--- check_urls.py
+++ check_urls.py
@@ -2,6 +2,13 @@ import csv
 
 import requests
 
+headers_win = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+headers = {
+    'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0'
+}
 
 with open('torch_urls.csv') as f:
     for row in csv.DictReader(f):
@@ -11,9 +18,7 @@ with open('torch_urls.csv') as f:
             if url == '':
                 print('NO_URL',',FALSE')
             else:
-                r = requests.head(url, timeout=2)
-                #print(r['Content-Length'])
-                #print(type(r))
+                r = requests.head(url, timeout=2, headers=headers)
                 #print(r.status_code)
                 if r.status_code == 200: 
                     print(url,',TRUE')
